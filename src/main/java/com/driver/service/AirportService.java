79697cb102eff0bab499e613f70b17a44d8456a8
Manoj183 <manojchari2000@gmail.com>
2024-07-06 12:22:57 +0530

    my solution

---
+++ src/main/java/com/driver/service/AirportService.java
@@ -0,0 +1,68 @@
+package com.driver.service;
+
+import com.driver.model.Airport;
+import com.driver.model.City;
+import com.driver.model.Flight;
+import com.driver.model.Passenger;
+import com.driver.repository.AirportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class AirportService {
+    @Autowired
+    AirportRepository repository;
+    public void addAirport(Airport airport){
+        repository.addAirport(airport);
+    }
+
+    public String getLargestAirportName() {
+        return repository.getLargestAirportName();
+    }
+
+    public void addFlight(@RequestBody Flight flight) {
+        repository.addFlight(flight);
+    }
+
+    public String getAirportNameFromFlightId(Integer flightId) {
+        return repository.getAirportNameFromFlightId(flightId);
+    }
+
+    public void addPassenger(Passenger passenger) {
+        repository.addPassenger(passenger);
+    }
+
+    public String bookATicket(Integer flightId, Integer passengerId) {
+        return repository.bookATicket(flightId,passengerId);
+    }
+
+    public String cancelATicket(Integer flightId, Integer passengerId) {
+        return repository.cancelATicket(flightId,passengerId);
+    }
+
+    public int calculateFlightFare(Integer flightId) {
+        return repository.calculateFlightFare(flightId);
+    }
+
+    public double getShortestDurationOfPossibleBetweenTwoCities(City fromCity, City toCity) {
+        return repository.getShortestDurationOfPossibleBetweenTwoCities(fromCity, toCity);
+    }
+
+    public int countOfBookingsDoneByPassengerAllCombined(Integer passengerId) {
+        return repository.countOfBookingsDoneByPassengerAllCombined(passengerId);
+    }
+
+    public int calculateRevenueOfAFlight(Integer flightId) {
+        return repository.calculateRevenueOfAFlight(flightId);
+    }
+
+    public int getNumberOfPeopleOn(Date date, String airportName) {
+        return repository.getNumberOfPeopleOn(date, airportName);
+    }
+}
